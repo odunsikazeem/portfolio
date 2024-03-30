54402ece4d296ce1a19ae5f04fd7a0b6df8b0fa6
odunsikazeem <145531096+odunsikazeem@users.noreply.github.com>
2024-03-30 19:25:40 +0000

    removed public directory

--- src/components/pages/projectlist.jsx
+++ src/components/pages/projectlist.jsx
@@ -2,7 +2,7 @@ const projectlist = [
   {
     id: 1,
     title: "Portfolio",
-    image: "/public/assets/images/portfolio.png",
+    image: "/assets/images/portfolio.png",
     projectURL: "https://akoredeprojects.netlify.app/",
     github: "https://github.com/odunsikazeem",
     info: "This project contains my professional profile, personal projects,social media accounts.I embarked on this project to showcase my dexterity in Frontend Development to potential employers,collaborators or clients that might need my services.",
@@ -10,7 +10,7 @@ const projectlist = [
   {
     id: 2,
     title: "Weather App",
-    image: "/public/assets/images/weather.png",
+    image: "/assets/images/weather.png",
     projectURL: "https://odunsikazeem.github.io/Weather-Dashboard/",
     github: "https://github.com/odunsikazeem",
     info: "The app uses openweathermap API to access weather data for a particular city and 5 day weather forecast.Application runs in the browser and feature dynamically updated HTML and CSS.Persistent data were stored in local storage for easy retrieval.",
@@ -18,7 +18,7 @@ const projectlist = [
   {
     id: 3,
     title: "Code Refactor",
-    image: "/public/assets/images/coderefactor.png",
+    image: "/assets/images/coderefactor.png",
     projectURL: "https://odunsikazeem.github.io/code-refactor/",
     github: "https://github.com/odunsikazeem",
     info: "The efficiency of the Horiseon website was improved upon.The project is my first attempt to refactor codes to acceptable standard in the industry",
@@ -26,7 +26,7 @@ const projectlist = [
   {
     id: 4,
     title: "Daily Planner",
-    image: "/public/assets/images/scheduler.png",
+    image: "/assets/images/scheduler.png",
     projectURL: "https://odunsikazeem.github.io/Daily-Planner-App/",
     github: "https://github.com/odunsikazeem",
     info: "This calender application helps you to plan your day by dynamically updating the time through color if the hour is past(Grey),present(Red) and future(Green).",
@@ -34,7 +34,7 @@ const projectlist = [
   {
     id: 5,
     title: "Viscorner Dashboard",
-    image: "/public/assets/images/viscorner.png",
+    image: "/assets/images/viscorner.png",
     projectURL: "https://spectacular-dusk-12cad8.netlify.app",
     github: "https://github.com/odunsikazeem",
     info: "This is an ongoing group project between my team members .The application serves as a tracking system for customer requests on the Viscorner company website. ",
@@ -42,7 +42,7 @@ const projectlist = [
   {
     id: 6,
     title: "Team Generator",
-    image: "/public/assets/images/TeamGenerator.png",
+    image: "/assets/images/TeamGenerator.png",
     projectURL: "https://github.com/odunsikazeem/Team-Profile-Generator //",
     github: "https://github.com/odunsikazeem",
     info: "The web application consists of resources and codes that allow users to generate a webpage that displays their team members basic information so that they can have quick access to their emails and GitHub profiles.", 
@@ -50,7 +50,7 @@ const projectlist = [
   {
     id: 7,
     title: "Password Generator",
-    image: "/public/assets/images/password.png",
+    image: "/assets/images/password.png",
     projectURL: "https://odunsikazeem.github.io/Password-Generator/",
     github: "https://github.com/odunsikazeem",
     info: "The application is an application for generating random passwords between 8-126 characters.",
@@ -58,7 +58,7 @@ const projectlist = [
   {
     id: 8,
     title: "Converter",
-    image: "/public/assets/images/converter.png",
+    image: "/assets/images/converter.png",
     projectURL: "https://pindoghd.github.io/Currencyexchange/",
     github: "https://github.com/odunsikazeem",
     info: "This is a group project.It contains a currency exchanger that uses two server API’s that allow users to convert a currency of their choice to a desired currency and it is also embeded with weather forecast application.",
@@ -67,7 +67,7 @@ const projectlist = [
   {
     id: 9,
     title: "Code Quiz",
-    image: "/public/assets/images/codeQuiz1.png",
+    image: "/assets/images/codeQuiz1.png",
     projectURL: "https://odunsikazeem.github.io/Code-Quiz/",
     github: "https://github.com/odunsikazeem",
     info: "Coding quiz application with multiple-choice questions with countdown timer that test your knowledge about JavaScript in 15 seconds.The application was designed to test my ability in using windows Application Programming Interface (API).",
@@ -76,7 +76,7 @@ const projectlist = [
   {
     id: 10,
     title: "ReadMe Generator",
-    image: "/public/assets/images/Generator.png",
+    image: "/assets/images/Generator.png",
     projectURL:
       "https://drive.google.com/file/d/1W0hD3iNBl3jYg7cPm-bhhS_JE7NocjkS/view",
     github: "https://github.com/odunsikazeem",
